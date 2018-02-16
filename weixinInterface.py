77c9bfff33729516ce3612d4c437058300fb6fa7
Zhang-EXT <uid38073@contiwan.com>
2018-02-16 18:01:08 +0800

    'h7'

--- weixinInterface.py
+++ weixinInterface.py
@@ -28,6 +28,7 @@ class WeixinInterface:
     def GET(self):
         print('GGGET')
         return render.hi()
+		
         return '<BODY><BR>First line~!@#$%^&*()_`-=+{}[];:<>?,./<BR>2nd Line<BR></BODY> '
         #获取输入参数
         data = web.input()
